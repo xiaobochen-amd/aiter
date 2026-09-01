2ed6bd50e0e81590feb766b2659d59627d0faa83
Yanyuan Qin <yanyuan.qin@amd.com>
2026-09-01 08:28:21 +0000

    iter-2: Delaying the inter-iteration LDS reuse barrier until immediately before

--- aiter/ops/flydsl/kernels/sparse_mla_prefill.py
+++ aiter/ops/flydsl/kernels/sparse_mla_prefill.py
@@ -272,6 +272,9 @@ def _compile_sparse_mla_prefill():
                 )
                 score = Vec(score_fragment.load().ir_value())
 
+                if key_start > fx.Int32(0):
+                    fx.barrier()
+
                 key_location = fx.Int32(_key_slot(tile, 0)) + key_slot_col
                 value_offset = key_location * _KV_LDS_PITCH
                 for chunk in range_constexpr(4):
@@ -442,7 +445,6 @@ def _compile_sparse_mla_prefill():
                         output_fragment,
                     )
                 accumulators[tile] = Vec(output_fragment.load().ir_value())
-            fx.barrier()
 
             loop_result = yield (
                 [_raw(running_max), _raw(running_sum)]
