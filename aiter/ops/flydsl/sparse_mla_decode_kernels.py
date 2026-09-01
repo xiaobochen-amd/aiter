52f882a7840311866a58d6347085fa24338f8c39
Yanyuan Qin <yanyuan.qin@amd.com>
2026-09-01 13:47:24 +0000

    iter-1: Extend the existing inner-iteration path to eight tiles only for suffici

--- aiter/ops/flydsl/sparse_mla_decode_kernels.py
+++ aiter/ops/flydsl/sparse_mla_decode_kernels.py
@@ -25,12 +25,14 @@ def _pick_inner_iter(seq: int, ng_total: int) -> int:
     """
     inner_iter = 1
     min_producer_ctas = 512
-    while inner_iter < 4:
+    while inner_iter < 8:
         candidate = inner_iter * 2
         if ng_total % candidate != 0:
             break
-        if seq * (ng_total // candidate) < min_producer_ctas:
-            break
+        candidate_ctas = seq * (ng_total // candidate)
+        if candidate_ctas < min_producer_ctas:
+            if candidate != 8 or candidate_ctas < 384:
+                break
         inner_iter = candidate
     return inner_iter
 
